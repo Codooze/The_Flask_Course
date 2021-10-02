f664fc66f106f220ecfd65b700186875fbca2d30
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-10-02 12:25:38 -0500

    FormsWithFlask

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request, redirect
 # contendra todas nuestra vistas
 
 # * you can create custom filter in a new file and import them into your __init__
@@ -57,3 +57,18 @@ def jinja():  # -Pasing python objects to the base template html
 @app.route("/about")  # todo
 def about():
     return render_template("public/about.html")
+
+
+@app.route('/sign_up', methods=["GET", "POST"])  # to use this get post you need to import requests
+def sing_up():
+    # get to get the content and post to post it into our server
+    # this code wiil execute only when the route recieve a post request
+    if request.method == "POST":
+        req = request.form
+        # here we have several alternative to store what's in the form inside variables:
+        username = req["username"]
+        email = req.get("email")
+        password = request.form["password"]
+        print(username, email, password)
+        return redirect(request.url)
+    return render_template("public/sign_up.html")
